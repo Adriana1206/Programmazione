3961652f291685584e1e482ddf26cdd582d82295
Adriana <piccioneadriana@gmail.com>
2023-12-24 01:39:34 +0100

    modifica JavaFullStack

---
+++ JavaFullStack/javajpa/src/test/java/com/example/javadb/dao/ClientiDAOTest.java
@@ -0,0 +1,59 @@
+package com.example.javadb.dao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.Test;
+
+import com.example.javadb.entities.Cliente;
+
+class ClientiDAOTest {
+	private static final Logger log = LogManager.getLogger(ClientiDAOTest.class);
+
+//	@Test
+	void testFindById() {
+		fail("Not yet implemented");
+	}
+
+	// @Test
+	void testFindAll() {
+		ClientiDAO cd = new ClientiDAO();
+		List<Cliente> clienti = cd.findAll();
+		assert(clienti.size() > 0);
+		for(Cliente c : clienti) {
+			log.debug(c);
+		}
+	}
+
+	@Test
+	void testCreate() {
+		ClientiDAO cd = new ClientiDAO();
+		Cliente cliente = new Cliente();
+		log.debug("Cliente vuoto {}", cliente);
+		cliente.setNome("Ronald");
+		cliente.setCognome("Regan");
+		cliente.setEmail("rr@gmail.com");
+		cliente.setIndirizzo("Via del boh, 54");
+		cliente.setCitta("Roma");
+		cliente.setProvincia("RM");
+		cliente.setCap("00100");
+		log.debug("Cliente pieno ma senza id {}", cliente);
+		cliente = cd.create(cliente);
+		assertNotNull( cliente.getIdCliente() );
+		log.debug("Cliente ricevuto da DAO {}", cliente);
+	}
+
+//	@Test
+	void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+//	@Test
+	void testDelete() {
+		fail("Not yet implemented");
+	}
+
+}
